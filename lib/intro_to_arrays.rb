ca112a33b81e4613a2b7dfd939512b25ee497a4b
Brooke Jones <bjones782@gmail.com>
2019-05-13 16:14:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -30,4 +30,5 @@ end
 
 def last_element_with_array_methods(last_country)
   @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-  last_country = @south_east_asia.pop
+  last_country = @south_east_asia.last
+end
