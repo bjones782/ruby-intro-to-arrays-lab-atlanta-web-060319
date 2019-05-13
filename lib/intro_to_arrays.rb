175f9f4d26f925ae96a064671a489313f55968d5
Brooke Jones <bjones782@gmail.com>
2019-05-13 16:01:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,3 +3,7 @@ def instantiate_new_array
   @my_new_array = instantiate_new_array
 end
 
+def array_with_two_elements
+  array_with_two_elements = [1, 2]
+  @my_two_array = array_with_two_elements
+end
